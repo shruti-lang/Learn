3393114413a2e4a3caac9bbf95757d89d8cb2b12
unknown <shruti.naavthi@gmail.com>
2024-08-07 15:24:33 +0530

    code modular

---
+++ Test/src/test/Test2.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2 {
+ static String browser;
+ static WebDriver driver;
+
+ public static void main(String[] args) {
+	 setBrowser();
+	 setBrowserConfig();
+	 runTest();
+
+}
+ 
+ public static void setBrowser() {
+  browser="Chrome";	
+}
+ 
+ public static void setBrowserConfig() {
+	  driver = new ChromeDriver();
+
+ }
+ 
+ public static void runTest() {
+		driver.get("https://www.ellucian.com/");
+		driver.quit();
+ }
+	
+}
